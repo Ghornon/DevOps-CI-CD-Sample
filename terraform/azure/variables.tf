1592c77d7d03ffd11b2999b05a6492bc034c28f7
Ghornon <szymon198198@gmail.com>
2025-11-18 22:16:52 +0100

    Add default variables to terraform

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,24 +1,29 @@
 variable "project_name" {
   description = "Project name"
   type        = string
+  default     = "ebilety"
 }
 
 variable "location" {
   description = "The Azure region to deploy resources in"
   type        = string
+  default     = "West Europe"
 }
 
 variable "app_subnet_address_prefixes" {
   type        = list(string)
   description = "The address prefixes to use for the application subnet"
+  default     = ["10.0.1.0/24"]
 }
 
 variable "admin_username" {
   description = "The admin username for the virtual machine"
   type        = string
+  default     = "adminuser"
 }
 
 variable "ssh_file_path" {
   description = "Path to the SSH public key file"
   type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
